e17ae6f48dd51e519065df79a7c937d2908240ff
Ivan <ivanfilho746@gmail.com>
2025-11-07 22:39:54 -0300

    Revert "feat: add css variables colors"
    This reverts commit dd1228c8a5e5ec8a9447097fd5ddb812f6628e90.

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,16 @@
-import { Controller, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import z from "zod";
-import { loginSchema } from "./zod/zod";
-import {
-  Field,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-} from "./components/ui/field";
-import { Button_Input } from "./components/ui/buttonInput";
-import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button.tsx"
 
-export default function App() {
-  const form = useForm<z.infer<typeof loginSchema>>({
-    defaultValues: {
-      // CPF: "",
-      password: "",
-      // name: "",
-      // telephone: 0,
-    },
-    resolver: zodResolver(loginSchema),
-  });
-
-  function onSubmit() {
-    console.log("super patapim");
-  }
-
-  return (
-    <div className="w-screen h-screen border-8">
-      <form onSubmit={form.handleSubmit(onSubmit)} className="">
-        <FieldGroup className="">
-          <Controller
-            control={form.control}
-            name="password"
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <FieldLabel className="font-medium text-sm">Nome</FieldLabel>
-                <Input {...field} id={field.name} className="border-seinfra-blue-light-200 rounded-3xl bg-white border-2  w-[90vw] sm:w-[600px] px-4 py-5" />
-                {fieldState.invalid && <FieldError errors={[{ message: "MEGA PATAPIM" }]} />}
-              </Field>
-            )}
-          />
-        </FieldGroup>
-      </form>
-    </div>
-  );
+function App() {
+    return (
+        <div className="flex min-h-svh flex-col items-center justify-center">
+            <Button className="m-2">Click me</Button>
+            {Array.from({ length: 5 }).map((_, i) => {
+                return (
+                    <Button key={i} className="m-2">Clica ai</Button>
+                )
+            })}
+        </div>
+    )
 }
+
+export default App
